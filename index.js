7316df495ea5b85411175c6c57fa3760241e6e4f
Saurabh Jain <saurabh4creative@gmail.com>
2022-12-21 02:59:20 +0530

    cors

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ dotenv.config();
 const express = require('express');
 const cors    = require('cors'); 
 const app     = express();
+
+var allowCrossDomain = function(req, res, next) {
+res.header("Access-Control-Allow-Origin", "*"); // allow requests from any other server
+res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); // allow these verbs
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
+     
+app.use(allowCrossDomain); // plumbing it in as middleware
+
+
+
 const PORT    = 8080 || process.env.PORT;
 const connDB  = require('./_config/db'); 
 const userRoutes = require('./_routes/userRoute');
 const projectRoutes = require('./_routes/projectRoute');
 const ticketRoutes = require('./_routes/ticketRoute');
+
+
 
 connDB();
 
