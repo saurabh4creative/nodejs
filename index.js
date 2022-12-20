1ded41f8c9252898450fe6bd29786c4cc8b5c96e
Saurabh Jain <saurabh4creative@gmail.com>
2022-12-21 02:06:41 +0530

    india

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ app.use(cors({
     origin: '*'
 }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/projects", projectRoutes);
 app.use("/api/v1/tickets", ticketRoutes);
