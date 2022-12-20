f7b56d6a46f31fbaf5a65d2f727b17ee57a56de5
Saurabh Jain <saurabh4creative@gmail.com>
2022-12-21 02:35:05 +0530

    dasdsad

--- index.js
+++ index.js
@@ -13,20 +13,7 @@ connDB();
 
 app.use(express.json());
 
-var permitCrossDomainRequests = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    // some browsers send a pre-flight OPTIONS request to check if CORS is enabled so you have to also respond to that
-    if ('OPTIONS' === req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-
-app.use(permitCrossDomainRequests);
+app.options('*', cors())
 
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/projects", projectRoutes);
